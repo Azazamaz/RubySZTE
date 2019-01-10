847396ac7ccf8ebfb50982f38f5fe15091eb6f80
Lajos Bodnar <azazamaz7@gmail.com>
2019-01-10 10:39:07 +0100

    JS format

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,11 +10,11 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-= require jquery
+    = require jquery
 //= require jquery3
 // = require popper
 // = require bootstrap
-= require rails-ujs
-= require jquery_ujs
-= require turbolinks
-= require_tree .
+    = require rails-ujs
+    = require jquery_ujs
+    = require turbolinks
+    = require_tree .
